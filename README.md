41f0cb38e0f5bfe751ccd08f39de4e054972b5a8
Christoph Grabenstein <christoph.grabenstein@qossmic.com>
2023-10-03 23:10:09 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# What is this?
+
+This is a small library that consists of an enum of possible keyboard keys. I wrote it to create a layer of abstraction between other libraries of mine. It might be useful to others too, so feel free to use it and create PRs for missing keys.
---
+++ src/Key.php
@@ -0,0 +1,80 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Grabenstein\Keys;
+
+enum Key
+{
+    case A;
+    case B;
+    case C;
+    case D;
+    case E;
+    case F;
+    case G;
+    case H;
+    case I;
+    case J;
+    case K;
+    case L;
+    case M;
+    case N;
+    case O;
+    case P;
+    case Q;
+    case R;
+    case S;
+    case T;
+    case U;
+    case V;
+    case W;
+    case X;
+    case Y;
+    case Z;
+
+    case ONE;
+    case TWO;
+    case THREE;
+    case FOUR;
+    case FIVE;
+    case SIX;
+    case SEVEN;
+    case EIGHT;
+    case NINE;
+    case ZERO;
+
+    case ARROW_LEFT;
+    case RETURN;
+    case SPACE;
+
+    case BACKSPACE;
+    case DELETE;
+
+    case TAB;
+    case ARROW_RIGHT;
+    case ARROW_UP;
+    case ARROW_DOWN;
+
+    case LEFT_SHIFT;
+    case RIGHT_SHIFT;
+    case LEFT_CONTROL;
+    case RIGHT_CONTROL;
+    case LEFT_ALT;
+    case RIGHT_ALT;
+    case CAPS_LOCK;
+    case FN;
+
+    case F1;
+    case F2;
+    case F3;
+    case F4;
+    case F5;
+    case F6;
+    case F7;
+    case F8;
+    case F9;
+    case F10;
+    case F11;
+    case F12;
+}
